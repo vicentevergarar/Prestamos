9889907ac818a100bbf415d2b496ca7caf9dddd5
VicenteVergara <vicente.vergarar@cecar.edu.co>
2025-09-19 09:11:48 -0500

    Proyecto inicial de préstamos

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",  // Ajusta según la estructura de tu proyecto
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
